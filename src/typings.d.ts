ccf4dbb915c4e62de3f1158328a94dd541800e84
Andres Gesteira <andres.gesteira@mcmakler.de>
2020-02-24 10:29:27 +0100

    refine rick and morty api typings

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,6 +1,6 @@
 declare module 'rickmortyapi' {
   export function getEndpoints(): () => any;
-  export function getCharacter<T>(opt: T): (item: string, opt: T) => any;
-  export function getLocation<T>(opt: T): (item: string, opt: T) => any;
-  export function getEpisode<T>(opt: T): (item: string, opt: T) => any;
+  export function getCharacter<T>(opt?: T): Promise<any>;
+  export function getLocation<T>(opt?: T): Promise<any>;
+  export function getEpisode<T>(opt?: T): Promise<any>;
 }
